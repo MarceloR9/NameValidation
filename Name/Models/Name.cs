2e000adc5d6eedead9da917c8348189f61aefd90
Marcelo Rodrigues <marcelo.mdsr09@gmail.com>
2023-09-28 22:52:39 -0300

    Adicionar arquivos de projeto.

---
+++ Name/Models/Name.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Name.Models
+{
+    public class Name
+    {
+        
+        public void ValidarName(string nameDigitado) 
+        {
+            
+
+            if (nameDigitado != "Marcelo")
+            {
+                Console.WriteLine("Nome incorreto! Digite novamente");
+            }
+
+            else 
+            {
+                Console.WriteLine("Seja bem vindo de volta!");
+            }
+        }
+    }
+}
---
+++ Name/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Name.Models;
+
+class Program
+{
+    static void Main()
+    {
+        bool nameCorreto = false;
+
+        while (!nameCorreto)
+        {
+            Console.WriteLine("Digite seu nome:");
+            string nameDigitado = Console.ReadLine();
+
+
+            if (nameDigitado == "Marcelo")
+            {
+                nameCorreto = true;
+                Console.WriteLine("Seja bem vindo de volta!");
+            }
+
+            else
+            {
+                Console.WriteLine("Nome incorreto! Digite novamente.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
